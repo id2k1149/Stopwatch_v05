ab313cba66e435284f92cb499cc101a85e5afe4b
id2k1149 <id2k1149@gmail.com>
2023-04-07 19:50:18 -0400

    Initial Commit

---
+++ Stopwatch_v05/Stopwatch_v05App.swift
@@ -0,0 +1,17 @@
+//
+//  Stopwatch_v05App.swift
+//  Stopwatch_v05
+//
+//  Created by Max Franz Immelmann on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Stopwatch_v05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
